aaea2100a6fb309abc9765ebb3718f0a4f606b4c
vivian waithera <vivyannekori@gmail.com>
2023-08-23 09:13:56 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python3
+# sequences.py
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    a, b = 0, 1
+    for _ in range(length):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    print(fib_sequence)
